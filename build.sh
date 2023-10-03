8d33e7e1b1161bb0f0848523b00504d635ee1745
Ibrahem alnumman <ibrahem.omari96@gmail.com>
2023-10-03 21:25:29 +0300

    Feat: build script enhancement

---
+++ build.sh
@@ -0,0 +1,1 @@
+cd .. && rm -rf mediaPlayerPlugin.zip && zip -r mediaPlayerPlugin.zip mediaPlayerPlugin/ -x "*/.*" -x "*/build.sh" -x "mediaPlayerPlugin/node_modules/*" -x "mediaPlayerPlugin/coverage/*"
